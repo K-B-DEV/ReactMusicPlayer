5e734bdea85fd3a72e8f10488ee760f2d52f939c
KyleBayer18 <kylebayerbusiness@gmail.com>
2022-06-08 21:27:56 -0400

    Submitting complete website.

---
+++ src/data.js
@@ -0,0 +1,98 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Higher",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-1024x1024.jpg",
+      artist: "Misha, Jussi Halme",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=15088",
+      color: ["#FBA29A", "#ED7F77"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Travelbag",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/1245c0271290a3196328c0cf4aaa910cd873dfe7-300x300.jpg",
+      artist: "mommy, Sleepy Fish",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=19058",
+      color: ["#FFFEFA", "#588ECE"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Nebulae",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/03/75adfe0661d06a9ea66d9c2e99b31e92ae450ebe-300x300.jpg",
+      artist: "cocabona",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=16071",
+      color: ["#FE8271", "#3FBBA3"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Lonely Waves",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/80be32197ca7971424bef5e4d0c6810e1d4334c6-300x300.jpg",
+      artist: "C Y G N",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=14981",
+      color: ["#E088DE", "#0B102D"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Ah Yeah",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/644dedb802e85f3eb700b8b091e729504b680a9c-300x300.jpg",
+      artist: "Psalm Trees, James Berkeley",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10548",
+      color: ["#F8EED7", "#F5D85F"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Point in Space and Time",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/01/453b13ae3c0e2eec280fcc6357800b825239806e-300x300.jpg",
+      artist: "Flawed Mangoes",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=12166",
+      color: ["#FFEDBF", "#2B9E99"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Blue View",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/03/e2cf41018e0c244f7f683dd6f6313551996a2952-300x300.jpg",
+      artist: "Leavv",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=16216",
+      color: ["#FDE2B2", "#D54B49"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Now",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-1024x1024.jpg",
+      artist: "Misha, Jussi Halme",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=15080",
+      color: ["#A99DAF", "#313558"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Barrows",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/7f102bdde417f6ead9a120b2b931449e5c12b4da-300x300.jpg",
+      artist: "Aarigod",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=13003",
+      color: ["#9FC075", "#617D40"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
